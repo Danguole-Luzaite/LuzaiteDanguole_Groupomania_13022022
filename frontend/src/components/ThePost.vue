58921545f5bf398f94ce117914247ee80533003c
Danguole Luzaite <danguole.luzaite@gmail.com>
2022-03-18 17:05:01 +0100

    Création d'une boucle pour afficher tous les posts créés sur la page d'accueil.

--- frontend/src/components/ThePost.vue
+++ frontend/src/components/ThePost.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container id="ThePost" fluid class="mb-5">
-    <v-card app max-width="900" max-height="750" class="mx-auto" outlined elevation="2">
+    <v-card app max-width="900" max-height="750" class="mx-auto mb-4" outlined elevation="2" v-for="post in posts.slice().reverse()" :key="post.postId">
       <v-row >
         <v-avatar size="44">
-          <v-img src='../assets/default_avatar.png'></v-img>
+          <v-img src='../assets/default_avatar.png' ></v-img>
         </v-avatar>
         <v-card-subtitle>User Prenom Nom</v-card-subtitle>
         <v-spacer></v-spacer>
@@ -20,11 +20,11 @@
 
       <v-card-text>
         <!-- place pour postTitle-->
-        <v-card-title>Titre du post</v-card-title>
+        <v-card-title>{{ post.postTitle }}</v-card-title>
         <!-- place pour postMessage-->
-        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</v-card-text>
+        <v-card-text>{{ post.postMessage }}</v-card-text>
         <!-- place pour postImage-->
-        <v-img contain max-height="200" max-width="300" src='../assets/default_image.jpg'></v-img>
+        <v-img contain max-height="200" max-width="300" src='../assets/default_image.jpg'><!-- {{ post.postImage }} --></v-img>
       </v-card-text>
       <v-divider class="mb-2"></v-divider>
       <!-- button icons pour aimer le post et postuler un comment-->
@@ -41,24 +41,38 @@
 </template>
 
 <script>
+// Axios
+const axios = require('axios');
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api/'
+});
 
 
 export default {
   name: 'ThePost',
 
-  data: () => ({
-   
-  }),
+  data () {
+    return {
+      posts: [{
+        postTitle: '',
+        postMessage: '',
+        postImage: '',
+      }]
+    }
 
-  components: {
-    
   },
 
-  methods:{
-
+  mounted() {
+    //Axios Api pour obtenir tous les users
+    instance.get('/posts')
+    .then(response => {
+      return this.posts = response.data;
+      //console.log(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   },
-
-
 }
 
 </script>
